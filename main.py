1cb415f4f5173a0143b587bf16114b9bf0db27e7
pecneb <pecneb@pop-os.localdomain>
2021-04-16 23:07:28 +0200

    Matrix Class:not working idk why

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Matrix(object):
+    def __init__(self, n, m,):
+        self.n = n
+        self.m = m
+        self.mtx = n[m[0]]
+
+def __main__():
+    print("Fuck")
+    
+
+
+if __name__ == __main__:
+    main()
